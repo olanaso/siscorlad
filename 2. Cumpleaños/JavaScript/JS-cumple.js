4e5fbffc577c8ef8e6eb146fc5dcf887340d622f
cmedi <cmedinallantirhuay@gmail.com>
2020-06-16 13:27:06 -0500

    Listo para revisar

--- 2. Cumpleaños/JavaScript/JS-cumple.js
+++ 2. Cumpleaños/JavaScript/JS-cumple.js
@@ -1,14 +1,26 @@
 $(document).ready(function () {
-    $('#boxslider').bxSlider({
-        auto: true,
-        speed: 500,
-        pager:false,
-        pause: 4000,
-        minSlides: 1,
-        maxSlides: 4,
-        slideWidth:400
+    
+    // PETICIONES AJAX
+    $.ajax({
+        type: 'GET',
+        url: "https://api.jsonbin.io/b/5ee8fca80e966a7aa36abd46",
+        success: function (response) {
+            response.forEach((element) => {
+                console.log(element.nombre+element.apellidos+element.date);
+                $('#boxslider').append('<li><img src="img/cumpleanos.png" alt="Imagen de pastel de cumpleaños"/><p class="nombre">'+element.nombre+'</p><p class="apellido">'+element.apellidos+'</p><p class="cumpleanos">Feliz cumpleaños</p><p class="date">'+element.date+'</p></li>');
+            });
+        }
     });
+    // CONFIGURACION DEL SLIDER
     setInterval(() => {
-        $('.bx-viewport').attr("style", "width: 100%; overflow: hidden; position: relative; height: 192px;"); 
+        $('#boxslider').bxSlider({
+            auto: true,
+            speed: 500,
+            pager:false,
+            pause: 4000,
+            minSlides: 1,
+            maxSlides: 4,
+            slideWidth:400
+        }); 
     }, 1000);
 });
