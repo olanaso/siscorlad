7d45af09495957ec7659ffbd8b4544248f56cc3e
cmedi <cmedinallantirhuay@gmail.com>
2020-06-18 16:40:11 -0500

    login funcionando - revisar

---
+++ 1. Login/src/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    var progress = $('#progress-circle').hide();
+    var error = $('#error').hide();
+    var vaciar = $('#vacio').hide();
+    var cuentas = [];
+
+    $.ajax({
+        type: "GET",
+        url: "https://api.jsonbin.io/b/5eebbbe72406353b2e0839d0",
+        data: "json",
+        success: function (response) {
+            cuentas = response;
+        }
+    });
+
+    $('input[type="submit"]').click(function (e) {
+        e.preventDefault();
+        for (var element in cuentas) {
+            if (cuentas[element].user == $('#input-user').val().trim() || cuentas[element].email == $('#input-user').val().trim() && cuentas[element].password == $('#input-password').val().trim()) {
+                progress.fadeIn();
+                error.hide();
+                vaciar.hide();
+                setTimeout(() => {
+                    progress.fadeOut();
+                }, 2500);
+                break;
+            } else if ("" == $('#input-user').val().trim() || "" == $('#input-user').val().trim() && "" == $('#input-password').val().trim()){
+                vaciar.show();
+                error.hide();
+            } else{
+                error.show()
+                vaciar.hide();
+            }
+        }
+    });
+});
