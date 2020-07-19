9383ec80880d7f0c3dacfdee0dad49672e43ae8b
Carlos Jhulyan <cmedinallantirhuay@gmail.com>
2020-07-18 23:07:22 -0500

    update

--- 8. formulario-librodereclamaciones/Scripts/index.js
+++ 8. formulario-librodereclamaciones/Scripts/index.js
@@ -1,6 +1,97 @@
 $(document).ready(function () {
     
 
-    //HTML
+    /*====================================
+    ==============JQUERY================*/
+
     $('#tabs').tabs();
+    //MASCARAD PARA LOS FORMULARIOS
+    $('#tipoDocumento').blur(function (e) { 
+        e.preventDefault();
+        $('#numeroDocumento').val("");
+        if ($(this).val() === 'none') {
+            $('#numeroDocumento').unmask();
+        }
+        if ($(this).val() === 'dni') {
+            $('#numeroDocumento').removeAttr('disabled');
+            $('#numeroDocumento').mask('00000000');
+        }
+        if ($(this).val() === 'carnet') {
+            $('#numeroDocumento').removeAttr('disabled');
+            $('#numeroDocumento').unmask();
+        }
+        if ($(this).val() === 'passport') {
+            $('#numeroDocumento').removeAttr('disabled');
+            $('#numeroDocumento').unmask();
+        }
+    });
+    $('#numeroDocumento').mask('00000000');
+    $('#celular').mask('(+51) 000 000 000');
+    $('#celularJ').mask('(+51) 000 000 000');
+    $('#telefonoFijo').mask('(000) 000 000');
+    $('#telefonoFijoJ').mask('(000) 000 000');
+    $('#numeroRuc').mask('00000000000');
+
+    //Validando campos de formulario
+    $('#form-natural').validate({
+        rules: {
+            numeroDocumento: {
+                required: true,
+                minlength: 8
+            },
+            apellidoPaterno:{
+                required: true
+            },
+            apellidoMaterno:{
+                required: true
+            },
+            nombres:{
+                required: true
+            },
+            celular:{
+                required: true
+            },
+            telefonoFijo:{
+                required: false
+            },
+            correoElectronico:{
+                required: true,
+                email: true
+            },
+
+            domicilio:{
+                required: true
+            },
+            mensaje:{
+                required: true
+            }
+        }
+    });
+    $('#form-juridica').validate({
+        rules: {
+            numeroRuc: {
+                required: true,
+                minlength: 11
+            },
+            razonSocial:{
+                required: true
+            },
+            celularJ:{
+                required: true
+            },
+            telefonoFijoJ:{
+                required: false
+            },
+            correoElectronicoJ:{
+                required: true,
+                email: true
+            },
+            domicilioJ:{
+                required: true
+            },
+            mensajeJ:{
+                required: true
+            }
+        }
+    });
 });
--- 9. formulario-mesadepartes/Scripts/index.js
+++ 9. formulario-mesadepartes/Scripts/index.js
@@ -1,6 +1,66 @@
 $(document).ready(function () {
     
 
-    //HTML
-    
+    /*===================================
+    ===========Cargar archivos=========*/
+    $('#customFileLang').change(function (e) { 
+        e.preventDefault();
+        var extPdf = /(.pdf)$/i;
+        var extDocx = /(.docx)$/i;
+        if (!extDocx.exec($(this)[0].value) && !extPdf.exec($(this)[0].value)) {
+            alert('Seleccione solo archivos pdf, docx, doc');
+            $(this)[0].value = ''
+            console.log($(this)[0].value);
+            return;
+        }
+
+        $('#customFileText').html($(this)[0].files[0].name);
+
+        console.log($(this)[0].value);
+        console.log($(this)[0].files[0].size);
+    });
+
+    /*===============================
+    ===========JQUERYS=============*/
+    //Mascaras
+    $('#celular').mask('(+51) 000 000 000');
+    $('#numeroHojas').mask('###0');
+    $('#numeroDocumento').mask('00000000');
+    //Validacion de los campos de form
+    $('#formulario').validate({
+        rules: {
+            numeroDocumento: {
+                required: true,
+                minlength: 8
+            },
+            apellidoPaterno:{
+                required: true
+            },
+            apellidoMaterno:{
+                required: true
+            },
+            nombres:{
+                required: true
+            },
+            celular:{
+                required: true
+            },
+            correoElectronico:{
+                required: true,
+                email: true
+            },
+            domicilio:{
+                required: true
+            },
+            customFileLang:{
+                required: true
+            },
+            numeroHojas:{
+                required: true
+            },
+            mensajeTramite:{
+                required: true
+            }
+        }
+    });
 });
